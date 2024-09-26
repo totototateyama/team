724fcc626ab878922ab2a7ff32056200ccb00c67
totototateyama <toto.tateyaman@gmail.com>
2024-09-26 13:58:56 +0900

    モデル&マイグレｰション

--- db/migrate/20240926033434_create_schedules.rb
+++ db/migrate/20240926033434_create_schedules.rb
@@ -2,6 +2,16 @@ class CreateSchedules < ActiveRecord::Migration[7.0]
   def change
     create_table :schedules do |t|
 
+      t.integer :status_id, null: false
+      t.date :match_day, null: false
+      t.integer :match_week_id, null: false
+      t.string :opponent, null: false
+      t.string :location, null: false
+      t.integer :start_time_id, null: false
+      t.integer :meeting_time_id, null: false
+      t.date :deadline, null: false
+      # t.references :user, null: false, foreign_key: true
+
       t.timestamps
     end
   end
