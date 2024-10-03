dfd5b709b8e870b3273b9eb8e8287a92134ef444
totototateyama <toto.tateyaman@gmail.com>
2024-10-03 15:19:39 +0900

    過去のスケジュールを降順に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
 
   def index
-    @users = Schedule.includes(:user).where('match_day <= ?', Date.today - 1)
+    @users = Schedule.includes(:user).order(match_day: :desc).where('match_day <= ?', Date.today - 1)
   end
 
   def show
