c244159af1b0902bca962d59b253c21db32c3714
totototateyama <toto.tateyaman@gmail.com>
2024-10-09 16:42:32 +0900

    一旦完成！

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -4,15 +4,34 @@ class SchedulesController < ApplicationController
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
-    @schedules = Schedule.includes(:user).where('match_day >= ?', Date.today)
+    @schedules = Schedule.includes(:user).order(match_day: :asc).where('match_day >= ?', Date.today)
   end
 
   def new
     @schedule = Schedule.new
   end
 
+
   def create
-    @schedule = Schedule.new(schedule_params)
+      # match_dayの各部分を取得
+      match_day_year = params[:schedule]["match_day(1i)"].to_i
+      match_day_month = params[:schedule]["match_day(2i)"].to_i
+      match_day_day = params[:schedule]["match_day(3i)"].to_i
+
+      # start_timeとmeeting_timeの各部分を取得
+      start_hour = params[:schedule]["start_time(4i)"].to_i
+      start_minute = params[:schedule]["start_time(5i)"].to_i
+      meeting_hour = params[:schedule]["meeting_time(4i)"].to_i
+      meeting_minute = params[:schedule]["meeting_time(5i)"].to_i
+
+      # DateTimeを生成
+      start_datetime = DateTime.new(match_day_year, match_day_month, match_day_day, start_hour, start_minute)
+      meeting_datetime = DateTime.new(match_day_year, match_day_month, match_day_day, meeting_hour, meeting_minute)
+
+      @schedule = Schedule.new(schedule_params)
+      @schedule.start_time = start_datetime
+      @schedule.meeting_time = meeting_datetime
+      
     if @schedule.save
       redirect_to root_path
     else
@@ -20,22 +39,13 @@ class SchedulesController < ApplicationController
     end
   end
 
+
   def show
     @comment = Comment.new
     @comments = @schedule.comments
   end
 
-  def edit
-  end
-
-  def update
-    if @schedule.update(schedule_params)
-      redirect_to schedule_path(@schedule)
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
+  
   def destroy
     if @schedule.destroy
       redirect_to root_path
@@ -44,15 +54,50 @@ class SchedulesController < ApplicationController
     end
   end
 
-  private
+
+  def edit
+    @schedule.start_time = @schedule.start_time - 9.hours
+    @schedule.meeting_time = @schedule.meeting_time - 9.hours
+  end
+
+
+  def update
+    # match_dayの各部分を取得
+    match_day_year = params[:schedule]["match_day(1i)"].to_i
+    match_day_month = params[:schedule]["match_day(2i)"].to_i
+    match_day_day = params[:schedule]["match_day(3i)"].to_i
+
+    # start_timeとmeeting_timeの各部分を取得
+    start_hour = params[:schedule]["start_time(4i)"].to_i
+    start_minute = params[:schedule]["start_time(5i)"].to_i
+    meeting_hour = params[:schedule]["meeting_time(4i)"].to_i
+    meeting_minute = params[:schedule]["meeting_time(5i)"].to_i
+
+    start_datetime = DateTime.new(match_day_year, match_day_month, match_day_day, start_hour, start_minute)
+    meeting_datetime = DateTime.new(match_day_year, match_day_month, match_day_day, meeting_hour, meeting_minute)
+
+    @schedule.start_time = start_datetime
+    @schedule.meeting_time = meeting_datetime
+
+  if @schedule.save
+      redirect_to schedule_path(@schedule)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+ private
 
   def schedule_params
     params.require(:schedule).permit(:status_id, :match_day, :match_week_id, :opponent, :location, :start_time, :meeting_time, :deadline).merge(user_id: current_user.id)
   end
 
+
   def set_schedule
     @schedule = Schedule.find(params[:id])
   end
+
 
   def contributor_confirmation
     redirect_to root_path unless current_user == @schedule.user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 
-
   def index
     @users = Schedule.includes(:user).order(match_day: :desc).where('match_day <= ?', Date.today - 1)
   end
 
+
   def show
   end
 
+  
 end
